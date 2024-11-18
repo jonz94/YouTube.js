ef895c5056120a865f2f5742f0201bae84d959c7
jonz94 <jody16888@gmail.com>
2024-11-18 08:53:08 +0800

    feat(parser): Update `LiveChatBanner` node
    * Handle cases when `data.viewerIsCreator` might be missing
    * Mark `background_type` might be null
    * Add `banner_type`
    * Add `banner_properties_is_ephemeral`
    * Add `banner_properties_auto_collapse_delay_seconds`

--- src/parser/classes/livechat/items/LiveChatBanner.ts
+++ src/parser/classes/livechat/items/LiveChatBanner.ts
@@ -12,16 +12,22 @@ export default class LiveChatBanner extends YTNode {
   viewer_is_creator: boolean;
   target_id: string;
   is_stackable: boolean;
-  background_type: string;
+  background_type: string | null;
+  banner_type: string;
+  banner_properties_is_ephemeral: boolean;
+  banner_properties_auto_collapse_delay_seconds: string | null;
 
   constructor(data: RawNode) {
     super();
     this.header = Parser.parseItem(data.header, LiveChatBannerHeader);
     this.contents = Parser.parseItem(data.contents);
     this.action_id = data.actionId;
-    this.viewer_is_creator = data.viewerIsCreator;
+    this.viewer_is_creator = Boolean(data.viewerIsCreator);
     this.target_id = data.targetId;
     this.is_stackable = data.isStackable;
-    this.background_type = data.backgroundType;
+    this.background_type = data.backgroundType || null;
+    this.banner_type = data.bannerType;
+    this.banner_properties_is_ephemeral = Boolean(data.bannerProperties?.isEphemeral);
+    this.banner_properties_auto_collapse_delay_seconds = data.bannerProperties?.autoCollapseDelay?.seconds || null;
   }
 }
