d51e85fa1a5b5f483e2d32da7501920d5250e02b
jonz94 <jody16888@gmail.com>
2024-12-11 16:18:13 +0800

    🚧 WIP: work in progress...

--- src/parser/classes/livechat/items/LiveChatMembershipItem.ts
+++ src/parser/classes/livechat/items/LiveChatMembershipItem.ts
@@ -9,16 +9,24 @@ export default class LiveChatMembershipItem extends YTNode {
 
   id: string;
   timestamp: number;
+  timestamp_usec: string;
+  header_primary_text: Text;
   header_subtext: Text;
+  message: Text;
   author: Author;
   menu_endpoint: NavigationEndpoint;
+  context_menu_accessibility_label: string;
 
   constructor(data: RawNode) {
     super();
     this.id = data.id;
     this.timestamp = Math.floor(parseInt(data.timestampUsec) / 1000);
+    this.timestamp_usec = data.timestampUsec;
+    this.header_primary_text = new Text(data.headerPrimaryText);
     this.header_subtext = new Text(data.headerSubtext);
+    this.message = new Text(data.message);
     this.author = new Author(data.authorName, data.authorBadges, data.authorPhoto, data.authorExternalChannelId);
     this.menu_endpoint = new NavigationEndpoint(data.contextMenuEndpoint);
+    this.context_menu_accessibility_label = data.contextMenuAccessibility.accessibilityData.label;
   }
 }
