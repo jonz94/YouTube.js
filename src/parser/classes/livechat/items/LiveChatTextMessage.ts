f488fbfeb93c7dbc79990d734361133dcb53152f
jonz94 <jody16888@gmail.com>
2024-12-27 15:10:36 +0800

    🚧 WIP: work in progress...

--- src/parser/classes/livechat/items/LiveChatTextMessage.ts
+++ src/parser/classes/livechat/items/LiveChatTextMessage.ts
@@ -2,6 +2,7 @@ import { type ObservedArray, YTNode } from '../../../helpers.js';
 import type { RawNode } from '../../../index.js';
 import { Parser } from '../../../index.js';
 import Button from '../../Button.js';
+import ButtonView from '../../ButtonView.js';
 import NavigationEndpoint from '../../NavigationEndpoint.js';
 import Author from '../../misc/Author.js';
 import Text from '../../misc/Text.js';
@@ -9,17 +10,19 @@ import Text from '../../misc/Text.js';
 export class LiveChatMessageBase extends YTNode {
   static type = 'LiveChatMessageBase';
 
+  id: string;
   message: Text;
   inline_action_buttons: ObservedArray<Button>;
   timestamp: number;
-  id: string;
+  timestamp_usec: number;
 
   constructor(data: RawNode) {
     super();
+    this.id = data.id;
     this.message = new Text(data.message);
     this.inline_action_buttons = Parser.parseArray(data.inlineActionButtons, Button);
     this.timestamp = Math.floor(parseInt(data.timestampUsec) / 1000);
-    this.id = data.id;
+    this.timestamp_usec = data.timestampUsec;
   }
 }
 
@@ -28,6 +31,8 @@ export default class LiveChatTextMessage extends LiveChatMessageBase {
 
   author: Author;
   menu_endpoint: NavigationEndpoint;
+  context_menu_accessibility_label: string;
+  before_content_buttons: ObservedArray<ButtonView>;
 
   constructor(data: RawNode) {
     super(data);
@@ -40,5 +45,7 @@ export default class LiveChatTextMessage extends LiveChatMessageBase {
     );
 
     this.menu_endpoint = new NavigationEndpoint(data.contextMenuEndpoint);
+    this.context_menu_accessibility_label = data.contextMenuAccessibility.accessibilityData.label;
+    this.before_content_buttons = Parser.parseArray(data.beforeContentButtons, ButtonView);
   }
 }
